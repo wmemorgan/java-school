6f1ec81669db7300e2c385248fb1f3c0a94113ce
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-06-24 19:27:46 +0800

    Configure default Swagger settings
    Configure Swagger to work with disabled spring.resources.add-mappings

---
+++ schools/src/main/java/com/lambdaschool/schools/config/Swagger2Config.java
@@ -0,0 +1,55 @@
+package com.lambdaschool.schools.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Configures the default Swagger Documentation
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2Config
+{
+    /**
+     * Configures what to document using Swagger
+     *
+     * @return A Docket which is the primary interface for Swagger configuration
+     */
+    @Bean
+    public Docket api()
+    {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors
+                        .basePackage("com.lambdaschool.schools"))
+                .paths(PathSelectors.regex("/.*"))
+                .build()
+                .apiInfo(apiEndPointsInfo());
+    }
+
+    /**
+     * Configures some information related to the Application for Swagger
+     *
+     * @return ApiInfo a Swagger object containing identification information for this application
+     */
+    private ApiInfo apiEndPointsInfo()
+    {
+        return new ApiInfoBuilder().title("Exception Handling Example")
+                .description("Demonstration of custom exception handling")
+                .contact(new Contact("Wilfred Morgan",
+                        "http://www.wilfredmorgan.com",
+                        "wilfred@wilfredmorgan.com"))
+                .license("MIT")
+                .licenseUrl("https://github.com/wmemorgan/java-school/blob/master/LICENSE")
+                .version("1.0.0")
+                .build();
+    }
+}
